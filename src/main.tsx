d6809f21c78dc9a392c405d29dc9a97d96f5670b
hemxanthh <hemanthkeerthi565@gmail.com>
2025-08-01 21:39:42 +0530

    Fix: Remove ErrorBoundary import to resolve build issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AuthProvider } from './contexts/AuthContext';
-import ErrorBoundary from './components/ErrorBoundary';
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ErrorBoundary>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>
 );
